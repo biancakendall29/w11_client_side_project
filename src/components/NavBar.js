53afd1aada7ec4206eb28e441087d73559fb99cf
Bianca Kendall <[D)>
2022-06-20 13:14:02 +0200

    initialise react-router-dom in nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,32 @@
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import HomeContainer from "../containers/HomeContainer";
+
 const NavBar = () => {
 
     return (
+        <>        
+        <Router>
+            <div className="nav">
+                <ul>
+                    <li><Link to='/'>Home</Link></li>  
+                    <li><Link to='/customers'>Customer Sign up/Log in</Link></li>
+                    <li><Link to='/dealers'>Dealer Sign up/Log in</Link></li>
+                    <li><Link to='/contact'>Contact Us</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                </ul>
+
+                <Routes>
+                    <Route path="/" element={<HomeContainer />}></Route>
+
+                </Routes>
+
+            </div>
+
+        </Router>
+
+
         <h1>Hello from NavBar!</h1>
+        </>
     );
 
 }
